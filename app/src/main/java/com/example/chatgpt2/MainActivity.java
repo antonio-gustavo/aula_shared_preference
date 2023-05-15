3c5f0256d7b0aa375ffa5f19b6caeb91a0e9d30a
Antônio Gustavo <antoniogustavo10@gmail.com>
2023-05-15 19:25:59 -0300

    feat: usa sharedpreference pra armazenar chave API

---
+++ app/src/main/java/com/example/chatgpt2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.chatgpt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.preference.PreferenceManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText mEditText = findViewById(R.id.editText);
+        ImageView mImageView = findViewById(R.id.imageView7);
+
+        SharedPreferences mSharedPreferences = getSharedPreferences("MyPrefsFile", MODE_PRIVATE);
+        String savedText = mSharedPreferences.getString("edit_text_preference", "Insira sua chave de API | Open API");
+        mEditText.setText(savedText);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textToSave = mEditText.getText().toString();
+                mSharedPreferences.edit().putString("edit_text_preference", textToSave).apply();
+                Toast.makeText(MainActivity.this, "Chave API salva com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        ImageView imageView = findViewById(R.id.imageView7);
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), Chat.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+}
